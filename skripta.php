c477b82a58dcd1bc38dababac82b3117e67c0f52
Matija-Aleksic <matijaaleksic22@gmail.com>
2023-06-22 13:34:07 +0200

    dodan echo podataka kod unosa u bazu

--- skripta.php
+++ skripta.php
@@ -16,4 +16,13 @@ move_uploaded_file($_FILES["pphoto"]["tmp_name"], $target_dir);
 $query = "INSERT INTO article (datum, naslov, sazetak, tekst, slika, kategorija, arhiva ) VALUES ('$date', '$title', '$about', '$content', '$picture', '$category', '$archive')";
 $result = mysqli_query($dbc, $query) or die('Error querying databese.');
 mysqli_close($dbc); 
+
+echo "<h2>Prikaz unesenih podataka:</h2>";
+echo "<p>Naslov: $title</p>";
+echo "<p>Sažetak: $about</p>";
+echo "<p>Tekst: $content</p>";
+echo "<p>Slika: $picture</p>";
+echo "<p>Kategorija: $category</p>";
+echo "<p>Spremiti u arhivu: $archive</p>";
+echo "<p>Datum: $date</p>";
 ?>
